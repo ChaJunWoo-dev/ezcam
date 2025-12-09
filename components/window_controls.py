a7c331fee848581145453d508eedafd1b6644502
ChaJunWoo <wnsdnxla123@gmail.com>
2025-12-10 02:53:11 +0900

    feat: 도움말 툴팁 버튼 추가

--- components/window_controls.py
+++ components/window_controls.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QWidget, QHBoxLayout, QPushButton
+from PyQt6.QtCore import Qt
 import qtawesome as qta
 
 class WindowControls(QWidget):
@@ -11,6 +12,17 @@ class WindowControls(QWidget):
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(0)
 
+        self.help_btn = QPushButton()
+        self.help_btn.setIcon(qta.icon("fa5s.question-circle", color="#2b2b2b"))
+        self.help_btn.setObjectName("helpButton")
+        self.help_btn.setFixedWidth(40)
+        self.help_btn.setToolTip(
+            "단축키:\n"
+            "• Ctrl + 마우스 드래그: 창 크기 조정\n"
+            "• ESC: 오버레이 모드 종료"
+        )
+        self.help_btn.setToolTipDuration(0)
+
         self.minimize_btn = QPushButton()
         self.minimize_btn.setIcon(qta.icon("fa5s.minus", color="#2b2b2b"))
         self.minimize_btn.setObjectName("minimizeButton")
@@ -21,6 +33,7 @@ class WindowControls(QWidget):
         self.close_btn.setObjectName("closeButton")
         self.close_btn.setFixedWidth(40)
 
+        layout.addWidget(self.help_btn)
         layout.addWidget(self.minimize_btn)
         layout.addWidget(self.close_btn)
 
